3e9713ed9628e291e1bd492292806aa3df3cd728
Forrest Bingham <bingham_5@live.com>
2020-04-15 16:26:15 -0600

    Finished Stocks, Recipes

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,42 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+    print(len(recipe), "-- Recipe ingredients --", len(ingredients))
+    at_least_one_batch = True
+    max_batches = 0
+    loops = 0
+    if len(recipe) != len(ingredients):
+        at_least_one_batch = False
 
+    if at_least_one_batch == True:
+        for x in recipe:
+            print(recipe[x], "-- Recipe[x]")
+            print(ingredients[x], " -- Ingredient[x]")
+            batch = ingredients[x] // recipe[x]
+            print(batch, "-- BATCH")
+            
+
+            if batch == 0:
+                at_least_one_batch = False
+            
+            elif loops == 0 or max_batches >= batch:
+                print(batch)
+                max_batches = batch
+                print(max_batches, "max batches")
+                loops += 1
+        print(max_batches)
+        return max_batches
+    
+    else:
+        return 0    
+    
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  # recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+  # ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  recipe = { 'milk': 2, 'sugar': 40, 'butter': 20 } 
+  ingredients = { 'milk': 5, 'sugar': 120, 'butter': 500 }
+
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,11 +1,36 @@
 #!/usr/bin/python
 
 import argparse
-
+prices = [1050, 270, 1540, 3800, 2]
 def find_max_profit(prices):
-  pass
+    current_min_price = 1000000000
+    current_max_profit = -1000000000
+    sale = 0
+    for x in range(0,len(prices)):
+        print(x, "---- Round in loop -----------")
+        print(prices[x], "-- prices[x]")
+        print(current_min_price)
+        if prices[x] <= current_min_price:
+            current_min_price = prices[x]
+            print(current_min_price, "<-- NEW MIN PRICE")
+        print(x, "next x should be x+1")
+        for y in range(x+1, len(prices)):
+            print("Buying stock for:", prices[x], "Selling for:", prices[y])
+            sale = prices[y] - prices[x]
+            y += 1
+            if sale >= current_max_profit:
+                current_max_profit = sale
+                print("New MAX PROFIT", current_max_profit)
 
+    return current_max_profit
 
+    # if current_max_profit > 0:
+    #     return current_max_profit
+    # else: 
+    #     return("There is no profit to be gained with these numbers")  
+        
+
+print(find_max_profit(prices))
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
   parser = argparse.ArgumentParser(description='Find max profit from prices.')
