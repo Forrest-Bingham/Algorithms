79af079d2c7c6c5b8fb302b20333878f1a343f2f
Forrest Bingham <bingham_5@live.com>
2020-04-16 17:09:08 -0600

    Finished MVP

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,9 +1,34 @@
 #!/usr/bin/python
 
 import sys
-
+rps = [["rock"], ["paper"], ["scissors"]]
 def rock_paper_scissors(n):
-  pass 
+    #Base case
+    if n == 0:
+        return [[]]
+
+    elif n == 1:
+        return rps
+
+    #create list of lists with correct length: 3**n
+    list = []
+
+    moves = rock_paper_scissors(n-1)  #Recursion step, taking moves from previous rps
+    print(moves, "-----> Moves from n-1. Length is: ", len(moves))
+    for x in moves:
+        print(x, "------ X")
+        for y in rps:
+            print(y, "--------------Y", len(rps))
+            newMove = x + y
+            print(newMove, "----------- Adding this move to big list")
+            list.append(newMove)
+
+    return list
+
+    #loop through the big list 
+
+    #each loop outputs a possible option for rps
+    
 
 
 if __name__ == "__main__":
